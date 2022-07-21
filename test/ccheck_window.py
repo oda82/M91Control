5ce326ff7f56807df5d75ab8bf34eda4c276695a
Dmitry Olyanich <107524373+oda82@users.noreply.github.com>
2022-07-21 18:41:03 +1000

    Add files via upload

--- test/ccheck_window.py
+++ test/ccheck_window.py
@@ -1,26 +1,26 @@
 from tkinter import *
 from tkinter import ttk
+
 import matplotlib.pyplot as plt
 import json
+
+from lakeshore import FastHall
 
 with open('cch_res_all.txt','r') as f:
     res_json = f.read()
     
 data = json.loads(res_json) # json -> dictionary
-def pprint(data):
-    """pretty print for dict class"""
-    if type(data) == dict: print(json.dumps(data, indent=2))
-    else: print(data)
 
 
 class Res_cch():
-    """ class for show the resalts of contact check in rectangular form"""
+    """ class for show the resalts of contact pairs check in rectangular form"""
     def __init__(self, root, num):
         self.root = root
-        self.num = num
+        self.num = num #number of contacr pair 0 to 3
+        #size of result windows
         self.WIDTH = 100
         self.HEIGHT = 200
-        
+        #number of contact
         self.point1 = None
         self.point2 = None
         
@@ -42,14 +42,14 @@ class Res_cch():
         self.power = []
         
         self.draw_window()
-        self.update_window(data)
+        self.update_window(data)#<-----change
         
     def update_window(self, data):
         """ take data type of dict, parse it and show in form"""
         # data["ContactPairIVResults"][i]["ContactPair"]['Point1']
         #clear contact color
         for contact in self.contacts:
-            contact.config(bg='blue')
+            contact.config(bg='SystemButtonFace')
         #set measurable contact
         self.point1 = data["ContactPairIVResults"][self.num]["ContactPair"]['Point1']-1
         self.contacts[self.point1].config(bg='green')
@@ -73,7 +73,7 @@ class Res_cch():
         
         self.current_overload = data["ContactPairIVResults"][self.num]["CurrentOverload"]
         self.lbl_current_overload['bg'] = 'red' if self.current_overload else 'green'
-        
+        #take points from json for different measurement
         self.current = []
         self.voltage = []
         self.resistance = []
@@ -84,13 +84,11 @@ class Res_cch():
             self.voltage.append(i_v["VoltageInVolts"])
             self.resistance.append(i_v["ResistanceInOhms"])
             self.power.append(i_v["CurrentInAmps"] * i_v["VoltageInVolts"])
-        
-        
     
     def draw_window(self):
 
-        self.frm_root = Frame(self.root, width=self.WIDTH, height=self.HEIGHT, bg='blue')
-        self.frm_root.pack(side=LEFT, padx=5, pady=5)
+        self.frm_root = Frame(self.root, width=self.WIDTH, height=self.HEIGHT, relief=GROOVE, borderwidth=3)#, bg='blue'
+        self.frm_root.pack(side=LEFT, padx=5, pady=5, ipadx=2, ipady=2)
         
         self.lbl_r_squared_pass = Label(self.frm_root, text='Rsq Pass', bg='red')
         self.lbl_r_squared_pass.grid(row=1, column=0, columnspan=2)
@@ -118,33 +116,32 @@ class Res_cch():
         self.btn_show_graph = Button(self.frm_root, text='Show Graph', command=self.show_graph).grid(row=11, column=0, columnspan=2, pady=5)
         
     def show_graph(self):
+        fig, axs = plt.subplots(3,1)
         # voltage vs current
-        plt.plot(self.current, self.voltage, 'o')
-        plt.plot(self.current, [self.f_slope*x+self.f_offset for x in self.current], '-')
-        plt.title('voltage vs current contacts {}-{}'.format(self.point1 + 1, self.point2 + 1))
-        plt.xlabel('current')
-        plt.ylabel('voltage')
-        plt.show()
+        points, = axs[0].plot(self.current, self.voltage, 'o')
+        points.set_label('V(I)')
+        points, = axs[0].plot(self.current, [self.f_slope*x+self.f_offset for x in self.current], '-')
+        points.set_label('{:.2f}*x+{:.6f}'.format(self.f_slope, self.f_offset))
+        axs[0].legend()
+        axs[0].set_ylabel('voltage')
+        axs[0].set_title('contacts {} - {}'.format(self.point1 + 1, self.point2 + 1))
         # resistance vs current
-        plt.plot(self.current, self.resistance, 'o')
-        plt.plot(self.current, [sum(self.resistance)/len(self.resistance) for i in range(len(self.resistance)) ], '-')
-        plt.title('resistance vs current contacts {}-{}'.format(self.point1 + 1, self.point2 + 1))
-        plt.xlabel('current')
-        plt.ylabel('resistance')
-        plt.show()
+        points, = axs[1].plot(self.current, self.resistance, 'o')
+        points.set_label('R(I)')
+        points, = axs[1].plot(self.current, [self.f_slope for i in range(len(self.resistance)) ], '-')
+        points.set_label('R = {:.2f} Ohm'.format(self.f_slope))
+        axs[1].legend()
+        axs[1].set_ylabel('resistance')
         # power vs current
-        plt.plot(self.current, self.power, 'o')
-        plt.title('power vs current contacts {}-{}'.format(self.point1 + 1, self.point2 + 1))
-        plt.xlabel('current')
-        plt.ylabel('power')
+        points, = axs[2].plot(self.current, self.power, 'o')
+        points.set_label('P(I)')
+        axs[2].legend()
+        axs[2].set_xlabel('current')
+        axs[2].set_ylabel('power')
         plt.show()
         pass
         
-        
-        
-          
-
-
+  
 class CCheck_win():
     # class for creating main contact check windows
     def __init__(self, root):
@@ -154,10 +151,12 @@ class CCheck_win():
         self.create_frm_setup()
         #add to root result form
         self.create_frm_result()
+        #add to root coment form with open and save buttons
+        self.create_frm_comment()
         
     def create_frm_setup(self):
         #create root setup form with widgets and put it to root
-        self.frm_setup = Frame(self.root, bg='red', width=100, height=100)
+        self.frm_setup = Frame(self.root, width=100, height=100)#, bg='red'
         self.frm_setup.pack()
         #row0
         Label(self.frm_setup, text='Performs a contact check measurement on contact pairs \n1-2, 2-3, 3-4, and 4-1 for a van der Pauw sample.').grid(row=0, column=0, columnspan=3, padx=10, pady=3)
@@ -205,30 +204,38 @@ class CCheck_win():
         Button(self.frm_setup, text='Contact Check').grid(row=11, column=0, sticky='e', padx=10, pady=3)
         ttk.Progressbar(self.frm_setup).grid(row=11, column=1)
         Button(self.frm_setup, text='Stop/Reset').grid(row=11, column=2, sticky='w', padx=10, pady=3)
-        
-        
-        pass
     
     def create_frm_result(self):
          #create root result form with widgets and put it to root
-        self.frm_result = Frame(self.root, bg='green', width=100, height=100)
+        self.frm_result = Frame(self.root)
         self.frm_result.pack()
         self.result = []
         for i in range(4):
             self.result.append(Res_cch(self.frm_result, i))
         
+    def create_frm_comment(self):
+        self.frm_comment = Frame(self.root, width=100, height=100)#, bg='green'
+        self.frm_comment.pack(fill=BOTH)
+        Label(self.frm_comment, text='Comment:').pack(anchor='w', padx=5)#, bg='red'
+        Text(self.frm_comment, height=5, width=30).pack(fill=BOTH, expand=True, padx=5, pady=5)
+        Button(self.frm_comment, text='Open').pack(side=LEFT, padx=5, pady=3)
+        Button(self.frm_comment, text='Save').pack(side=LEFT, padx=5, pady=3)
+#-------------------END OF MAIN WINDOW class CCheck_win()-----------------------
+    
+class CCheck_ctrl():
+    def __init__(self, view):
+        self.view = view
         pass
-
-
-
-
+    
+    pass
 
 
 if __name__ == '__main__':
     root = Tk()
     root.title('Contackt check (Van der Pauw)')
     #root.geometry('640x480')
-    
-    cch = CCheck_win(root)
+
+    cch_view = CCheck_win(root)
+    cch_ctrl = CCheck_ctrl(cch_view)
 
     root.mainloop()
